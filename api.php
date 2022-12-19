3e64334219548d7279d9f1b94ed9a84dc9d7a854
andreadldev <andrea.delisi2@gmail.com>
2022-12-19 09:45:08 +0100

    commenta encode/decode

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php 
 $disc_list = file_get_contents("dischi.json");
-$disc_list = json_decode($disc_list, true);
-
-$json_string = json_encode($disc_list);
+// $disc_list = json_decode($disc_list, true);
+// $json_string = json_encode($disc_list);
 header("Content-Type: application/json");
-echo $json_string;
+// echo $json_string;
+echo $disc_list;
 ?>
